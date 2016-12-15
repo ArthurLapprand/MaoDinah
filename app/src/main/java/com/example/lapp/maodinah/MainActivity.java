8a555750bd34b83ee176c64cb137f0ddd4f93670
Lasalvia <Lasalvia>
2016-12-15 15:34:53 -0300

    Tirando codigos antigos, e comentários desnecessários

--- app/src/main/java/com/example/lapp/maodinah/MainActivity.java
+++ app/src/main/java/com/example/lapp/maodinah/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity  {
     }
 
     public boolean onTouchEvent(MotionEvent event){
-        if(!boo) {
+        if(!boo) { // Para caso tocar na tela uma vez, mensagem aleatória é retornada
             int temp = (int) (Math.random() * 10);
             TextView view = new TextView(this);
             view.setTextSize(50);
@@ -63,71 +63,33 @@ public class MainActivity extends Activity  {
 
 
             setContentView(view);
-            boo = true;
+            boo = true; // Para que só seja possivel ler as linhas da imagem apenas 1x
         }
         return false;
     }
-
+    //Metodo para calcular distancia euclidiana
     private double distEuclidiana(Point p1, Point p2){
 
         return Math.sqrt(Math.abs(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y))));
     }
-
+//Metodo principal do projeto
     public void helloworld() {
-        // make a mat and draw something
-        //File f = new File("/sdcard/Download/");
-        //String[] paths = f.list();
+        //Recebendo imagem original e guardando em original_image
         Mat original_image = Imgcodecs.imread(Environment.getExternalStorageDirectory().getPath() + "/Download/palma2.jpg", Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
-        Mat palm = original_image.clone();
-      //  Imgproc.cvtColor(palm, palm, Imgproc.COLOR_RGBA2GRAY);
-//        Mat binary =new Mat();
-//        Imgproc.threshold(palm, binary, 0,255, Imgproc.THRESH_BINARY);
-//        Mat mask = new Mat();
-//        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(20, 20));
-//        Imgproc.morphologyEx(binary, mask, Imgproc.MORPH_OPEN, kernel);
-//        Imgproc.threshold(mask, mask, 128,255, Imgproc.THRESH_BINARY_INV );
-//        Mat blurred = new Mat();
-//        Imgproc.GaussianBlur(palm, blurred, new Size(9, 9), 0);
-//        Imgproc.threshold(blurred, binary,0, 255, Imgproc.THRESH_OTSU );
-//        Mat result = new Mat();
-//        binary.copyTo(result,mask);
-//        Imgproc.equalizeHist(palm, palm);
-//        Imgproc.GaussianBlur(palm, palm, new Size(9, 9), 5);
-//        Imgproc.equalizeHist(palm, palm);
-////
-//        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50));
-//        //Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_BLACKHAT, kernel, new Point(25, 25), 2);
-//
+        Mat palm = original_image.clone(); // Clonando original_image
+        //Kernel sendo criado para utilização do morphologyEx
        Mat kernel2 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(4, 4));
+        // transformando a imagem para preto e branco/ cinza
         Imgproc.cvtColor(palm,palm, Imgproc.COLOR_BGRA2GRAY);
-        //Imgproc.GaussianBlur(original_image,original_image, new Size(5,5),3);
-        //Imgproc.threshold(original_image,original_image,1,255,Imgproc.THRESH_BINARY);
+        //Aplicando o gradient e o threshold na copia da imagem original
         Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_GRADIENT, kernel2, new Point(2, 2), 10);
        Imgproc.threshold(palm, palm, 20, 255,Imgproc.THRESH_BINARY);
-//        Imgproc.threshold(palm ,palm,120,255,Imgproc.THRESH_OTSU + Imgproc.THRESH_BINARY_INV);
-//        Imgproc.equalizeHist(palm, palm);
-//        Imgproc.morphologyEx(result, result, Imgproc.MORPH_OPEN, kernel2, new Point(2, 2), 13);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_OPEN, kernel2, new Point(2, 2), 13);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_OPEN, kernel2, new Point(2, 2), 13);
-//        Imgproc.morphologyEx(result, result, Imgproc.MORPH_CLOSE, kernel2, new Point(2, 2), 15);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_BLACKHAT, kernel2, new Point(2, 2), 50);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_DILATE, kernel2, new Point(2, 2), 20);
-//      Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_CLOSE, kernel2, new Point(2, 2), 20);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_DILATE, kernel2, new Point(2, 2), 20);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_CLOSE, kernel2, new Point(2, 2), 20);
-//        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_BLACKHAT, kernel2, new Point(2, 2), 10);
-        //Imgproc.Laplacian(palm, palm, CvType.CV_8U, 1, 100, 0);
 
-//CANNY
         Mat edges = new Mat();
-  //      Imgproc.Canny(original_image, original_image, 30, 60);
-        //Mat palma = original_image.clone();
-        //Imgproc.cvtColor(palma, palma, Imgproc.COLOR_RGB2GRAY);
-        //dst.setTo(new Scalar(0));
-        //palm.copyTo(dst, edges);
-
-//        //HoughLinesP
+        //HoughLinesP
+        // Utilização do metodo da HoughLines probabilistica para detectar as linhas
         Imgproc.HoughLinesP(palm, edges, 1, Math.PI /180 , 100, 100, 20);
+        //pontos e arrays de pontos sendo declarados para seperação das principais linhas (Linha da Vida, Linha do Amor, Linha da cabeça
         Point[] pi = new Point[edges.rows()];
         Point[] pf = new Point[edges.rows()];
         Point[] res = new Point[edges.rows()];
@@ -141,29 +103,25 @@ public class MainActivity extends Activity  {
         Point[] resultanteaf = new Point[edges.rows()];
         Point[] resultantebf = new Point[edges.rows()];
         Point[] resultantecf = new Point[edges.rows()];
+        // Auxiliares para armazenar a quantidade de pontos em diferentes regiões
         int aux = 0;
         int aux1 = 0;
         int aux2 = 0;
         int aux3 = 0;
+        //Variaveis sendo declaradas para saber o tamanho aproximadamente das 3 principais linhas.
         double min1x = original_image.width();
         double max1x = 0;
         double min2x = original_image.width();
         double max2x = 0;
         double min3x = original_image.width();
         double max3x = 0;
-
         double min1y = original_image.height();
         double max1y = 0;
         double min2y = original_image.height();
         double max2y = 0;
         double min3y = original_image.height();
         double max3y = 0;
-
-
-        Imgproc.line(original_image, pontocima , pontocima, new Scalar(0,255,0),50);
-        Imgproc.line(original_image, pontomeio , pontomeio, new Scalar(0,255,0),50);
-        Imgproc.line(original_image, pontobaixo , pontobaixo, new Scalar(0,255,0),50);
-        //Imgproc.line(original_image, pontomeio , pontomeio, new Scalar(0,255,0),50);
+        //Pegando os pontos da tela
         for (int i = 0; i < edges.rows(); i++) {
             double[] vec = edges.get(i, 0);
             double  x1 = vec[0],
@@ -173,18 +131,20 @@ public class MainActivity extends Activity  {
             Point start = new Point(x1, y1);
             Point end = new Point(x2, y2);
             Point medio = new Point((x1+x2)/2,(y1+y2)/2);
-            //if (Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)) > 400)
+            // Algumas condições para limitar a região que as linhas aparecem
             if (y1 < (palm.height()*0.7) && y2 < (palm.height()*0.7) &&
                 y1 > (palm.height()*0.15) && y2 > (palm.height()*0.15)) {
-                //Imgproc.line(original_image, start, end, new Scalar(255, 0, 0), 3);
+                //Desenhando as linhas
+                Imgproc.line(original_image, start, end, new Scalar(255, 0, 0), 3);
                 pm[aux] = medio;
-
+                //Calculando a distancia dos pontos que satisfazem as condições para os pontos fixos para detecção de cada linha
                 double a,b,c;
                 a = distEuclidiana(pm[aux],pontobaixo);
                 b = distEuclidiana(pm[aux],pontomeio);
                 c = distEuclidiana(pm[aux],pontocima);
                 aux = aux+1;
-                //BAIXO
+                //Calculando os valores min e max de cada uma das 3 principais linhas
+                //BAIXO - Linha da vida
                 if((a < b) &&( a < c)){
                     resultanteai[aux1] = start;
                     resultanteaf[aux1] = end;
@@ -198,7 +158,7 @@ public class MainActivity extends Activity  {
                     if(start.y > max1y) max1y = start.y;
                     if(end.y > max1y) max1y = end.y;
                     aux1 = aux1+1;
-                } //CIMA
+                } //CIMA - Linha do coração
                  else if((c < b) &&( c < a)){
                     resultantebi[aux2] = start;
                     resultantebf[aux2] = end;
@@ -212,7 +172,7 @@ public class MainActivity extends Activity  {
                     if(start.y > max2y) max2y = start.y;
                     if(end.y > max2y) max2y = end.y;
                     aux2 = aux2+1;
-                } // MEIO
+                } // MEIO - Linha da cabeça
                 else {
                     resultanteci[aux3] = start;
                     resultantecf[aux3] = end;
@@ -231,31 +191,24 @@ public class MainActivity extends Activity  {
 
             }
         }
-
+//criando os pontos que os valores max e min das principais linhas
         Point p1 = new Point(min1x,min1y);
         Point p2 = new Point(min2x,min2y);
         Point p3 = new Point(min3x,min3y);
         Point p4 = new Point(max1x,max1y);
         Point p5 = new Point(max2x,max2y);
         Point p6 = new Point(max3x,max3y);
-        Imgproc.line(original_image,p1, p4, new Scalar(255,0,0), 3);
-        Imgproc.line(original_image,p2, p5, new Scalar(255,0,0), 3);
-        Imgproc.line(original_image,p3, p6, new Scalar(255,0,0), 3);
+        //Se quiser imprimir linhas que tem o tamanho das 3 principais linhas da mão.
+//        Imgproc.line(original_image,p1, p4, new Scalar(255,0,0), 3);
+//        Imgproc.line(original_image,p2, p5, new Scalar(255,0,0), 3);
+//        Imgproc.line(original_image,p3, p6, new Scalar(255,0,0), 3);
 
+        // Comentado... mudar o resultante se quiser aparecer só 1 linha( e comentar a linha 138)
 //        for(int i = 0; i < aux3; i++){
 //       Imgproc.line(original_image,resultanteci[i], resultantecf[i], new Scalar(255,0,0), 3);
 //        }
 
-//
-//        for (int i = 0; i < aux; i++){
-//        res[i].x = pf[i].x - pi[i].x;
-//        res[i].y = pf[i].y - pi[i].y;
-//            if()
-//        }
-//
-        //for(int i = 0; i )
-        //Imgproc.cvtColor(original_image,original_image,Imgproc.COLOR_RGB2GRAY);
-        //Imgproc.cvtColor(palm,palm,Imgproc.COLOR_RGB2GRAY);
+
         // convert to bitmap:
         Bitmap bm = Bitmap.createBitmap(palm.cols(), palm.rows(),Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(original_image, bm);
