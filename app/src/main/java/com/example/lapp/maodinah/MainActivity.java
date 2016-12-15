7755ff9e5edbb2206cf6b97367f3bf948c241fdc
Lasalvia <Lasalvia>
2016-12-14 23:25:05 -0300

    separação de linhas ok para uma imagem

--- app/src/main/java/com/example/lapp/maodinah/MainActivity.java
+++ app/src/main/java/com/example/lapp/maodinah/MainActivity.java
@@ -20,6 +20,7 @@ import org.opencv.imgproc.Imgproc;
 public class MainActivity extends Activity  {
 
     static boolean initialized;
+    static boolean boo = false;
     static {
         if (!OpenCVLoader.initDebug()) {
             // do something
@@ -39,29 +40,36 @@ public class MainActivity extends Activity  {
     }
 
     public boolean onTouchEvent(MotionEvent event){
-        int temp = (int)(Math.random() * 10);
-        TextView view = new TextView(this);
-        view.setTextSize(50);
-        if(temp == 1){
-            view.setText( "Você vai morrer de velhice" );
-        } else if(temp == 2){
-            view.setText("Você ganhará na loteria mas a tristeza ainda lhe perseguirá");
-        }else if(temp == 3){
-            view.setText("Sua vida estará repleta de felicidades e tristezas");
-        }else if(temp == 4){
-            view.setText("Você só pensa no ontem.. a partir de 2020 começará a pensar no amanhã!");
-        }else if(temp == 5){
-            view.setText("Você encontrará alguem muito especial em sua vida que lhe ajudará nos momentos mais dificeis");
-        }else if(temp == 6){
-            view.setText("Você ira desistir de seus principais objetivos por causa de problemas pessoais ");
-        }else if(temp == 7){
-            view.setText("Sua linha da vida afirma que viverá bastante!");
-        }else view.setText("Sua linha da vida diz que você ira viver pouco");
+        if(!boo) {
+            int temp = (int) (Math.random() * 10);
+            TextView view = new TextView(this);
+            view.setTextSize(50);
+            if (temp == 1) {
+                view.setText("Você vai morrer de velhice");
+            } else if (temp == 2) {
+                view.setText("Você ganhará na loteria mas a tristeza ainda lhe perseguirá");
+            } else if (temp == 3) {
+                view.setText("Sua vida estará repleta de felicidades e tristezas");
+            } else if (temp == 4) {
+                view.setText("Você só pensa no ontem.. a partir de 2020 começará a pensar no amanhã!");
+            } else if (temp == 5) {
+                view.setText("Você encontrará alguem muito especial em sua vida que lhe ajudará nos momentos mais dificeis");
+            } else if (temp == 6) {
+                view.setText("Você ira desistir de seus principais objetivos por causa de problemas pessoais ");
+            } else if (temp == 7) {
+                view.setText("Sua linha da vida mandou avisar que você irá viver bastante!");
+            } else view.setText("Sua linha da vida diz que você ira viver pouco");
 
 
-        setContentView(view);
-
+            setContentView(view);
+            boo = true;
+        }
         return false;
+    }
+
+    private double distEuclidiana(Point p1, Point p2){
+
+        return Math.sqrt(Math.abs(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y))));
     }
 
     public void helloworld() {
@@ -90,11 +98,11 @@ public class MainActivity extends Activity  {
 //        //Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_BLACKHAT, kernel, new Point(25, 25), 2);
 //
        Mat kernel2 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(4, 4));
-        Imgproc.cvtColor(original_image,original_image, Imgproc.COLOR_BGRA2GRAY);
+        Imgproc.cvtColor(palm,palm, Imgproc.COLOR_BGRA2GRAY);
         //Imgproc.GaussianBlur(original_image,original_image, new Size(5,5),3);
         //Imgproc.threshold(original_image,original_image,1,255,Imgproc.THRESH_BINARY);
-        Imgproc.morphologyEx(original_image, original_image, Imgproc.MORPH_GRADIENT, kernel2, new Point(2, 2), 10);
-       Imgproc.threshold(original_image, original_image, 20, 255,Imgproc.THRESH_BINARY);
+        Imgproc.morphologyEx(palm, palm, Imgproc.MORPH_GRADIENT, kernel2, new Point(2, 2), 10);
+       Imgproc.threshold(palm, palm, 20, 255,Imgproc.THRESH_BINARY);
 //        Imgproc.threshold(palm ,palm,120,255,Imgproc.THRESH_OTSU + Imgproc.THRESH_BINARY_INV);
 //        Imgproc.equalizeHist(palm, palm);
 //        Imgproc.morphologyEx(result, result, Imgproc.MORPH_OPEN, kernel2, new Point(2, 2), 13);
@@ -116,9 +124,30 @@ public class MainActivity extends Activity  {
         //Imgproc.cvtColor(palma, palma, Imgproc.COLOR_RGB2GRAY);
         //dst.setTo(new Scalar(0));
         //palm.copyTo(dst, edges);
-//
+
 //        //HoughLinesP
-        Imgproc.HoughLinesP(original_image, edges, 1, Math.PI / 180, 20, 100, 100);
+        Imgproc.HoughLinesP(palm, edges, 1, Math.PI /180 , 100, 100, 20);
+        Point[] pi = new Point[edges.rows()];
+        Point[] pf = new Point[edges.rows()];
+        Point[] res = new Point[edges.rows()];
+        Point[] pm = new Point[edges.rows()];
+        Point pontobaixo = new Point((palm.width()*0.17),(palm.height()*0.6));
+        Point pontomeio = new Point((palm.width()*0.5),(palm.height()*0.5));
+        Point pontocima = new Point((palm.width()*0.7),(palm.height())*0.2);
+        Point[] resultanteai = new Point[edges.rows()];
+        Point[] resultantebi = new Point[edges.rows()];
+        Point[] resultanteci = new Point[edges.rows()];
+        Point[] resultanteaf = new Point[edges.rows()];
+        Point[] resultantebf = new Point[edges.rows()];
+        Point[] resultantecf = new Point[edges.rows()];
+        int aux = 0;
+        int aux1 = 0;
+        int aux2 = 0;
+        int aux3 = 0;
+        Imgproc.line(original_image, pontocima , pontocima, new Scalar(0,255,0),50);
+        Imgproc.line(original_image, pontomeio , pontomeio, new Scalar(0,255,0),50);
+        Imgproc.line(original_image, pontobaixo , pontobaixo, new Scalar(0,255,0),50);
+        //Imgproc.line(original_image, pontomeio , pontomeio, new Scalar(0,255,0),50);
         for (int i = 0; i < edges.rows(); i++) {
             double[] vec = edges.get(i, 0);
             double  x1 = vec[0],
@@ -127,17 +156,56 @@ public class MainActivity extends Activity  {
                     y2 = vec[3];
             Point start = new Point(x1, y1);
             Point end = new Point(x2, y2);
+            Point medio = new Point((x1+x2)/2,(y1+y2)/2);
             //if (Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)) > 400)
             if (y1 < (palm.height()*0.7) && y2 < (palm.height()*0.7) &&
-                y1 > (palm.height()*0.15) && y2 > (palm.height()*0.15)   )
-                Imgproc.line(palm, start, end, new Scalar(255,0,0), 3);
+                y1 > (palm.height()*0.15) && y2 > (palm.height()*0.15)) {
+                //Imgproc.line(original_image, start, end, new Scalar(255, 0, 0), 3);
+                pm[aux] = medio;
 
+                double a,b,c;
+                a = distEuclidiana(pm[aux],pontobaixo);
+                b = distEuclidiana(pm[aux],pontomeio);
+                c = distEuclidiana(pm[aux],pontocima);
+                aux = aux+1;
+                //BAIXO
+                if((a < b) &&( a < c)){
+                    resultanteai[aux1] = (start);
+                    resultanteaf[aux1] = end;
+                    aux1 = aux1+1;
+                } //CIMA
+                 else if((c < b) &&( c < a)){
+                    resultantebi[aux2] = start;
+                    resultantebf[aux2] = end;
+                    aux2 = aux2+1;
+                } // MEIO
+                else {
+                    resultanteci[aux3] = start;
+                    resultantecf[aux3] = end;
+                    aux3 = aux3+1;
+                }
+
+
+            }
         }
+
+        for(int i = 0; i < aux2; i++){
+       Imgproc.line(original_image,resultantebi[i], resultantebf[i], new Scalar(255,0,0), 3);
+        }
+
+//
+//        for (int i = 0; i < aux; i++){
+//        res[i].x = pf[i].x - pi[i].x;
+//        res[i].y = pf[i].y - pi[i].y;
+//            if()
+//        }
+//
+        //for(int i = 0; i )
         //Imgproc.cvtColor(original_image,original_image,Imgproc.COLOR_RGB2GRAY);
         //Imgproc.cvtColor(palm,palm,Imgproc.COLOR_RGB2GRAY);
         // convert to bitmap:
         Bitmap bm = Bitmap.createBitmap(palm.cols(), palm.rows(),Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(palm, bm);
+        Utils.matToBitmap(original_image, bm);
 
         // find the imageview and draw it!
         ImageView iv = (ImageView) findViewById(R.id.activity_main);
